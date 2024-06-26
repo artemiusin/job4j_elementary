8aab709002d2224177fb215cf6695408b9498a08
Artem Iusin <ysin.artem@gmail.com>
2024-06-26 14:44:25 +0300

    6.0.3. Заполнение массива. [#226300 #376669]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,6 +6,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Artem";
+        names[1] = "Yusin";
+        names[2] = "Leyna";
+        names[3] = "Vanya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
