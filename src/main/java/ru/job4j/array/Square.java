61ee94e6df1166d0f7e1d6155f6393de113258fb
Artem Iusin <ysin.artem@gmail.com>
2024-06-27 10:34:11 +0300

    6.0.7. Массивы в тестах. [#226789 #376676]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -4,7 +4,7 @@ public class Square {
 
     public static int[] calculate(int bound) {
         int[] result = new int[bound];
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < bound; i++) {
             result[i] = i * i;
         }
         return result;
